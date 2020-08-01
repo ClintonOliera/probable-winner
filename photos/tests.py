a1952f70049b52931e536904705d718093ee9266
clintonoliera <clintonoliera391@gmail.com>
2020-08-02 00:21:17 +0300

    this commit will add a test to test image uploading

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Image,Location,Category
 
 # Create your tests here.
+class TestImage(TestCase):
+    def setUp(self):
+        self.location = Location(name='Naivasha')
+        self.location.save_location()
+
+        self.category = Category(name='home')
+        self.category.save_category()
+
+        self.image_test = Image(id=1, name='image', description='this is a testing image', location=self.location,
+                                category=self.category)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_test, Image))                            
