c000db6fadcbc376a2b7f6f3ac2269b50f474aac
clintonoliera <clintonoliera391@gmail.com>
2020-08-02 00:23:31 +0300

    this commit will test saving the image to our database

--- photos/tests.py
+++ photos/tests.py
@@ -13,4 +13,9 @@ class TestImage(TestCase):
         self.image_test = Image(id=1, name='image', description='this is a testing image', location=self.location,
                                 category=self.category)
     def test_instance(self):
-        self.assertTrue(isinstance(self.image_test, Image))                            
+        self.assertTrue(isinstance(self.image_test, Image)) 
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        after = Image.objects.all()
+        self.assertTrue(len(after) > 0)                               
